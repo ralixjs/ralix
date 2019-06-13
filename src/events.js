1a016c574b9af258f49a2c1c6489a3ca58682e2c
Marc Anguera Insa <srmarc.ai@gmail.com>
2019-06-13 15:17:53 +0200

    minor refactor on Events (smaller functions)

--- src/events.js
+++ src/events.js
@@ -11,28 +11,30 @@ export default class Events {
 
   bind() {
     findAll(this.availableEvents.join(', ')).forEach((element) => {
-      let attributes = Array.from(element.attributes)
-
-      let events = attributes.forEach((attr) => {
+      Array.from(element.attributes).forEach((attr) => {
         let match = attr.name.match(this.eventsRegex)
 
         if (match) {
           const [eventType, listener] = match
 
-          const originalEvent = element[eventType]
-          element[eventType] = null
-
-          element.addEventListener(listener, (event) => {
-            if (listener == 'click')
-              event.preventDefault()
-
-            App.currentElement = element
-            App.currentEvent   = event
-
-            originalEvent.call()
-          })
+          this._addListener(element, eventType, listener)
         }
       })
+    })
+  }
+
+  _addListener(element, eventType, listener) {
+    const originalEvent = element[eventType]
+    element[eventType] = null
+
+    element.addEventListener(listener, (event) => {
+      if (listener == 'click')
+        event.preventDefault()
+
+      App.currentElement = element
+      App.currentEvent   = event
+
+      originalEvent.call()
     })
   }
 }
