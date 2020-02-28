b02cb1732f9cc25b1e7e61a394c245cc376c2a7c
Marc Anguera Insa <srmarc.ai@gmail.com>
2020-02-28 17:43:25 +0100

    refactor to use array destructuring assignment

--- src/core.js
+++ src/core.js
@@ -137,15 +137,15 @@ export default class Core {
         return el.setAttribute(attribute, value)
     else if (typeof attribute  === 'object')
       Object.entries(attribute).forEach(entry => {
-        const k = entry[0];
-        const v = entry[1];
-        el.setAttribute(k, v)
-      });
+        const [key, value] = entry
+        el.setAttribute(key, value)
+      })
   }
 
   data(query, attribute, value) {
     const el = _element(query)
     if (!el) return
+
     if (attribute === undefined && value === undefined) return el.dataset
 
     if (typeof attribute === 'string')
@@ -155,9 +155,8 @@ export default class Core {
         return el.dataset[attribute] = value
     else if (typeof attribute  === 'object')
       Object.entries(attribute).forEach(entry => {
-        const k = entry[0]
-        const v = entry[1]
-        return el.dataset[k] = v
+        const [key, value] = entry
+        el.dataset[key] = value
       })
   }
 
