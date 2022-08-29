47c72e1c19829dc4d65fd83984f45e31d5608ba0
jfelip937 <joelfelipis7@gmail.com>
2022-08-29 19:41:32 +0200

    Add Attributes tests

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -36,27 +36,27 @@ describe('Visibility', () => {
     test('with element', () => {
       show(element);
 
-      expect(element.style.cssText).toBe("");
+      expect(element.style.cssText).toBe('');
     });
 
     test('with query', () => {
       show('#target');
 
-      expect(element.style.cssText).toBe("");
+      expect(element.style.cssText).toBe('');
     });
 
     test('with array of elements', () => {
       show([element, element2]);
 
-      expect(element.style.cssText).toBe("");
-      expect(element2.style.cssText).toBe("");
+      expect(element.style.cssText).toBe('');
+      expect(element2.style.cssText).toBe('');
     });
 
     test('with query targeting multiple elements', () => {
       show('.to-be-shown');
 
-      expect(element.style.cssText).toBe("");
-      expect(element2.style.cssText).toBe("");
+      expect(element.style.cssText).toBe('');
+      expect(element2.style.cssText).toBe('');
     });
   });
 
@@ -213,4 +213,107 @@ describe('Classes', () => {
       expect(hasClass('.extra-class', 'extra-class')).toBeTruthy();
     });
   });
+});
+
+describe('Attributes', () => {
+  beforeEach(() => {
+    element.setAttribute('id', 'elem_1');
+    element2.setAttribute('id', 'elem_2');
+  });
+
+  describe('attr', () => {
+    test('getter', () => {
+      expect(attr(element, 'id')).toBe('elem_1');
+      expect(attr('#elem_2', 'id')).toBe('elem_2');
+    });
+
+    describe('setter', () => {
+      test('with attribute', () => {
+        attr(element, 'id', 'elem_3');
+
+        expect(attr(element, 'id')).toBe('elem_3');
+      })
+
+      test('with object', () => {
+        attr(element, { id: 'elem_4', disabled: true });
+
+        expect(attr(element, 'id')).toBe('elem_4');
+        expect(attr(element, 'disabled')).toBeTruthy();
+      })
+    });
+  });
+
+  describe('removeAttr', () => {
+    beforeEach(() => {
+      element.setAttribute('disabled', true);
+    });
+
+    test('with attribute', () => {
+      removeAttr(element, 'disabled');
+
+      expect(element.getAttribute('disabled')).toBeNull();
+    })
+
+    test('with array', () => {
+      removeAttr(element, ['id', 'disabled']);
+
+      expect(element.getAttribute('disabled')).toBeNull();
+      expect(element.getAttribute('id')).toBeNull();
+    })
+  })
+
+  describe('data', () => {
+    test('getter', () => {
+      element.dataset.attribute = 'test';
+      element.dataset.test = 'attribute';
+
+      expect(Object.assign({}, data(element))).toEqual({ attribute: 'test', test: 'attribute' });
+      expect(data(element, 'attribute')).toBe('test');
+    })
+
+    describe('setter', () => {
+      test('with attribute', () => {
+        data(element, 'attribute', 'test');
+
+        expect(element.dataset.attribute).toBe('test');
+      })
+
+      test('with object', () => {
+        data(element, { attribute: 'test', test: 'attribute' });
+
+        expect(element.dataset.attribute).toBe('test');
+        expect(element.dataset.test).toBe('attribute');
+      })
+    });
+  })
+
+  describe('removeData', () => {
+    beforeEach(() => {
+      element.dataset.attr1 = 'test1';
+      element.dataset.attr2 = 'test2';
+      element.dataset.attr3 = 'test3';
+    });
+
+    test('with attribute', () => {
+      removeData(element, 'attr1');
+
+      expect(element.dataset.attr1).toBeUndefined();
+    })
+
+    test('with array', () => {
+      removeData(element, ['attr1', 'attr2']);
+
+      expect(element.dataset.attr1).toBeUndefined();
+      expect(element.dataset.attr2).toBeUndefined();
+      expect(element.dataset.attr3).toBe('test3');
+    })
+
+    test('all', () => {
+      removeData(element);
+
+      expect(element.dataset.attr1).toBeUndefined();
+      expect(element.dataset.attr2).toBeUndefined();
+      expect(element.dataset.attr3).toBeUndefined();
+    })
+  })
 });
